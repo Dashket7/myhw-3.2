0edf07b225ae9b75cbbe8094b613375a20611a4d
Artemeva D <dashartemeva7@gmail.com>
2023-07-15 16:37:20 +0500

    new commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,8 @@
 public class BmiService {
+    public double calculate(int weight, double height) {
+        double result = weight / (height * height);
+
+        return result;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        BmiService service = new BmiService();
+        int weight = 55;
+        double height = 1.65;
+        double result = service.calculate(weight, height);
+        System.out.println(result);
     }
 }
